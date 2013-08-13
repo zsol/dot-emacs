e282bd14118c1444c1ff63f3d05f001d5a0dceb1
Zsolt Dollenstein <zsol.zsol@gmail.com>
2013-08-13 21:39:50 +0200

    import stuff from https://github.com/purcell/emacs.d

---
+++ emacs.d.symlink/elpa/wgrep-ag-20130725.1831/wgrep-ag-autoloads.el
@@ -0,0 +1,30 @@
+;;; wgrep-ag-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (wgrep-ag-setup) "wgrep-ag" "wgrep-ag.el" (21002
+;;;;;;  34184 0 0))
+;;; Generated autoloads from wgrep-ag.el
+
+(autoload 'wgrep-ag-setup "wgrep-ag" "\
+
+
+\(fn)" nil nil)
+(add-hook 'ag-mode-hook 'wgrep-ag-setup)
+
+;;;***
+
+;;;### (autoloads nil nil ("wgrep-ag-pkg.el") (21002 34184 369672
+;;;;;;  0))
+
+;;;***
+
+(provide 'wgrep-ag-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; wgrep-ag-autoloads.el ends here
